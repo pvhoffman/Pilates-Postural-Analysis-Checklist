6b130bbbfc1401033be3985e3e2f3c8d498bd4f3
Paul Hoffman <p.hoffman@yellowmagic.com>
2014-12-19 09:57:24 -0800

    sway back essential mat work all layers

--- Pilates Postural Analysis Checklist/PACSwayBackEssentialMatworkLayer.m
+++ Pilates Postural Analysis Checklist/PACSwayBackEssentialMatworkLayer.m
@@ -24,30 +24,107 @@ typedef struct pac_swayback_essential_matwork_s {
     int is_bold;
 }pac_swayback_essential_matwork_t;
 
-static pac_swayback_essential_matwork_t pac_swayback_essential_matwork_layer1_warmup[] = {
-        {"", "", 1}
-      , {0, 0, -1}
-};
-static pac_swayback_essential_matwork_t pac_swayback_essential_matwork_layer1_exercises[] = {
-        {"", "", 1}
+static pac_swayback_essential_matwork_t pac_swayback_essential_matwork_layer1_warmup[] = { 
+
+    {"Breathing", "", 1}
+      , {"Imprint & Release", "", 1}
+      , {"Hip Release", "", 1}
+      , {"Supine Spinal Rotation", "", 1}
+      , {"Cat Stretch", "", 1}
+      , {"Hip Rolls", "", 1}
+      , {"Scapula Isolation", "", 1}
+      , {"Arm Circles", "", 1}
+      , {"Head Nods", "", 1}
+      , {"Elevation & Depression of Scapulae", "", 1}
       , {0, 0, -1}
 };
 
-static pac_swayback_essential_matwork_t pac_swayback_essential_matwork_layer2_warmup[] = {
-        {"", "", 1}
+static pac_swayback_essential_matwork_t pac_swayback_essential_matwork_layer1_exercises[] = { 
+    {"Ab Prep", "hands behind head", 1}
+      , {"Breast Stroke Preps 1 2", "", 1}
+      , {"Shell Stretch", "", 1}
+      , {"Half Roll Back", "", 1}
+      , {"One Leg Circle", "both knees bent", 1}
+      , {"Spine Twist", "one a pillow, cross-legged, or raised mat", 1}
+      , {"Rolling Like a Ball Prep", "", 1}
+      , {"Obliques", "prep, tabletop legs, less than 90°", 1}
+      , {"Spine Stretch Forward", "on a pillow, cross-legged, or pillow or arc", 1}
+      , {"Single Leg Extension", "", 1}
+      , {"Swan Dive Prep", "", 1}
       , {0, 0, -1}
 };
-static pac_swayback_essential_matwork_t pac_swayback_essential_matwork_layer2_exercises[] = {
-        {"", "", 1}
+
+static pac_swayback_essential_matwork_t pac_swayback_essential_matwork_layer2_warmup[]    = {
+
+    {"Breathing", "", 0}
+      , {"Imprint & Release", "", 0}
+      , {"Hip Release", "", 0}
+      , {"Supine Spinal Rotation", "", 0}
+      , {"Cat Stretch", "", 0}
+      , {"Hip Rolls", "", 0}
+      , {"Scapula Isolation", "", 0}
+      , {"Arm Circles", "", 0}
+      , {"Head Nods", "", 0}
+      , {"Elevation & Depression of Scapulae", "", 0}
+      , {0, 0, -1}
+};
+static pac_swayback_essential_matwork_t pac_swayback_essential_matwork_layer2_exercises[] = { 
+    {"Ab Prep", "arms reaching", 1}
+      , {"Breast Stroke Preps 1 2", "", 0}
+      , {"Shell Stretch", "", 0}
+      , {"Hundred", "tabletop legs, less than 90°", 1}
+      , {"Half Roll Back", "", 0}
+      , {"Roll Up", "half", 1}
+      , {"One Leg Circle", "bottom leg straight", 1}
+      , {"Spine Twist", "on a pillow, cross-legged, or pillow or arc", 0}
+      , {"Rolling Like a Ball Prep", "", 0}
+      , {"Single Leg Stretch", "", 1}
+      , {"Obliques", "prep, tabletop legs, less than 90°", 0}
+      , {"Neck Pull Prep", "knees bent", 1}
+      , {"Obliques Roll Back", "", 1}
+      , {"Spine Stretch Forward", "on a pillow, cross-legged, or pillow or arc", 0}
+      , {"Single Leg Extension", "", 0}
+      , {"Swan Dive Prep", "", 0}
+      , {"Leg Pull Front Prep", "", 1}
       , {0, 0, -1}
 };
 
 static pac_swayback_essential_matwork_t pac_swayback_essential_matwork_layer3_warmup[] = {
-        {"", "", 1}
+    {"Breathing", "", 0}
+      , {"Imprint & Release", "", 0}
+      , {"Hip Release", "", 0}
+      , {"Supine Spinal Rotation", "", 0}
+      , {"Cat Stretch", "", 0}
+      , {"Hip Rolls", "", 0}
+      , {"Scapula Isolation", "", 0}
+      , {"Arm Circles", "", 0}
+      , {"Head Nods", "", 0}
+      , {"Elevation & Depression of Scapulae", "", 0}
       , {0, 0, -1}
 };
-static pac_swayback_essential_matwork_t pac_swayback_essential_matwork_layer3_exercises[] = {
-        {"", "", 1}
+static pac_swayback_essential_matwork_t pac_swayback_essential_matwork_layer3_exercises[] = { 
+    {"Ab Prep", "", 0}
+      , {"Breast Stroke Preps 1 2", "", 0}
+      , {"Shell Stretch", "", 0}
+      , {"Hundred", "feet down, neutral pelvis", 1}
+      , {"Half Roll Back", "", 0}
+      , {"Roll Up", "full", 1}
+      , {"One Leg Circle", "bottom leg straight", 0}
+      , {"Spine Twist", "on a pillow, cross legged, or pillow or arc", 0}
+      , {"Rolling Like a Ball", "full", 1}
+      , {"Single Leg Stretch", "", 0}
+      , {"Obliques", "feet down, neutral pelvis", 1}
+      , {"Double Leg Stretch", "", 1}
+      , {"Roll Over Prep", "", 1}
+      , {"Breast Stroke", "", 1}
+      , {"Neck Pull Prep", "knees bent", 0}
+      , {"Obliques Roll Back", "", 0}
+      , {"Side Kicks", "", 1}
+      , {"Side Leg Lift Series", "", 1}
+      , {"Spine Stretch Forward", "on a pillow, cross-legged, on pillow or arc", 0}
+      , {"Single Leg Extension", "", 0}
+      , {"Swan Dive Prep", "with legs", 0}
+      , {"Leg Pull Front Prep", "", 0}
       , {0, 0, -1}
 };
 
